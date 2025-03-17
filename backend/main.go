faa904eb2555b8d61433efb089d7bc7623298bd2
youssup <youssupsong@gmail.com>
2025-03-16 20:27:23 -0400

    feat: Connecting PostgreSQL database

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -25,6 +27,8 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"message": "Pong!"})
 	})
 
+	dbConnection()
+
 	// Start the server on port 8080
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -32,4 +36,29 @@ func main() {
 	}
 	fmt.Println("Server running on port", port)
 	r.Run(":" + port)
+}
+
+func dbConnection() {
+	databaseUser := os.Getenv("DATABASE_USER")
+	databasePassword := os.Getenv("DATABASE_PASSWORD")
+	databaseName := os.Getenv("DATABASE_NAME")
+	databaseHost := os.Getenv("DATABASE_HOST")
+	databasePort := os.Getenv("DATABASE_PORT")
+
+	databaseURL := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", databaseUser, databasePassword, databaseHost, databasePort, databaseName)
+	conn, err := pgx.Connect(context.Background(), databaseURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
 }
